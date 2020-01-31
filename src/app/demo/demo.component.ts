0cdc852a59ee02da43b2e958afc68d8dc8a8d57c
srsoumya96 <sr.soumya96@gmail.com>
2020-01-31 11:17:55 +0530

    1 file modified

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -31,33 +31,33 @@ import {
         opacity: 0
       })),
       transition('in1 => out1', [
-        animate('2s', style({
+        animate('1s', style({
         transform: 'translateX(100%)',
         opacity: 0
       }))
       ]),
       transition('in2 => out2', [
-        animate('2s', style({
+        animate('1s', style({
         transform: 'translateX(100%)',
         opacity: 0
       }))
       ]),
       transition('in3 => out3', [
-        animate('2s', style({
+        animate('1s', style({
         transform: 'translateX(100%)',
         opacity: 0
       }))
       ]),
-      transition('out3 => in3', [
-        animate('2s', style({
+      transition('out1 => in1', [
+        animate('1s', style({
       }))
       ]),
       transition('out2 => in2', [
-        animate('2s', style({
+        animate('1s', style({
       }))
       ]),
-      transition('out1 => in1', [
-        animate('2s', style({
+      transition('out3 => in3', [
+        animate('1s', style({
       }))
       ])
     ])
@@ -72,7 +72,7 @@ export class DemoComponent implements OnInit {
   isIn3 = false;
   showAnime = true;
 
-  myfun1() {
+    myfun1() {
     this.isIn1 = !this.isIn1;
     setTimeout(() => {
     this.myfun2();
@@ -80,23 +80,43 @@ export class DemoComponent implements OnInit {
     }
     myfun2() {
       this.isIn2 = !this.isIn2;
-    setTimeout(() => {
-    this.myfun3();
-    }, 2000);
+    if(this.isIn2 == true) {
+      setTimeout(() => {
+      this.myfun3();
+      }, 2000);
     }
-    myfun3() {
-      this.isIn3 = !this.isIn3;
+    else {
       setTimeout(() => {
     this.myfun1();
     }, 2000);
     }
+    }
+    myfun3() {
+      this.isIn3 = !this.isIn3;
+      if(this.isIn3 == true) {
+      setTimeout(() => {
+      this.myfun3();
+      }, 2000);
+      }
+      else {
+        setTimeout(() => {
+      this.myfun2();
+      }, 2000);
+    }
+    }
 
-  showAnimation() {
-    this.showAnime = false;
+    showAnimation() {
+    // this.showAnime = false;
     setTimeout(() => {
     this.myfun1();
     }, 2000);
-  }
+    // while(this.showAnime == false) {
+    //   this.isIn1 = !this.isIn1;
+    //   this.isIn2 = !this.isIn2;
+    //   this.isIn3 = !this.isIn3;
+    // }
+
+    }
   
   ngOnInit() {
     // setInterval(() => {
