c7d6921896922a04021f1e50fcba321a2ee45f01
srsoumya96 <sr.soumya96@gmail.com>
2020-01-31 11:27:10 +0530

    2 files modified

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -31,13 +31,13 @@ import {
         opacity: 0
       })),
       transition('in1 => out1', [
-        animate('1s', style({
+        animate('1s 2s', style({
         transform: 'translateX(100%)',
         opacity: 0
       }))
       ]),
       transition('in2 => out2', [
-        animate('1s', style({
+        animate('1s 1s', style({
         transform: 'translateX(100%)',
         opacity: 0
       }))
@@ -49,7 +49,7 @@ import {
       }))
       ]),
       transition('out1 => in1', [
-        animate('1s 2s', style({
+        animate('1s', style({
       }))
       ]),
       transition('out2 => in2', [
@@ -57,7 +57,7 @@ import {
       }))
       ]),
       transition('out3 => in3', [
-        animate('1s', style({
+        animate('1s 2s', style({
       }))
       ])
     ])
@@ -70,64 +70,13 @@ export class DemoComponent implements OnInit {
   isIn1 = false;
   isIn2 = false;
   isIn3 = false;
-  showAnime = false;
-
-  //   myfun1() {
-  //   this.isIn1 = !this.isIn1;
-  //   setTimeout(() => {
-  //   this.myfun2();
-  //   }, 2000);
-  //   }
-  //   myfun2() {
-  //     this.isIn2 = !this.isIn2;
-  //   if(this.isIn2 == true) {
-  //     setTimeout(() => {
-  //     this.myfun3();
-  //     }, 2000);
-  //   }
-  //   else {
-  //     setTimeout(() => {
-  //   this.myfun1();
-  //   }, 2000);
-  //   }
-  //   }
-  //   myfun3() {
-  //     this.isIn3 = !this.isIn3;
-  //     if(this.isIn3 == true) {
-  //     setTimeout(() => {
-  //     this.myfun3();
-  //     }, 2000);
-  //     }
-  //     else {
-  //       setTimeout(() => {
-  //     this.myfun2();
-  //     }, 2000);
-  //   }
-  //   }
-
-  //   showAnimation() {
-  //   // this.showAnime = false;
-  //   setTimeout(() => {
-  //   this.myfun1();
-  //   }, 2000);
-    // while(this.showAnime == false) {
-    //   this.isIn1 = !this.isIn1;
-    //   this.isIn2 = !this.isIn2;
-    //   this.isIn3 = !this.isIn3;
-    // }
-
-    // }
   
   ngOnInit() {
     setInterval(() => {
     this.isIn1 = !this.isIn1;
-    }, 6000);
-    setInterval(() => {
-      this.isIn2 = !this.isIn2;
-    }, 6000);
-    setInterval(() => {
-      this.isIn3 = !this.isIn3;
-    }, 6000);
+    this.isIn2 = !this.isIn2;
+    this.isIn3 = !this.isIn3;
+    }, 4000);
   }
 
 }
